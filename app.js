6bc3835bc667d9db896fa758164354abbb198cd6
jvmMachado <joaovmmachado@gmail.com>
2020-02-21 19:10:55 -0300

    add mongoose and setup to work with mongodb instead of arrays

--- app.js
+++ app.js
@@ -4,6 +4,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const _ = require('lodash');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const postSchema = new mongoose.Schema ({
+  title: {
+    type: String,
+    required: [true, 'Please, enter a title for your post']
+  },
+  content: {
+    type: String,
+    required: [true, 'Please, type some content for your post']
+  }
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+
 
 const homeStartingContent =
   'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.';
@@ -12,17 +36,15 @@ const aboutContent =
 const contactContent =
   'Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.';
 
-const posts = [];
-
-const app = express();
-
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
 app.get('/', function(req, res) {
-  res.render('home', { pageContent: homeStartingContent, postsList: posts });
+
+    Post.find({},(err, posts) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('home', { pageContent: homeStartingContent, postsList: posts });
+    }
+  });
 });
 
 app.get('/about', function(req, res) {
@@ -37,22 +59,26 @@ app.get('/compose', function(req, res) {
   res.render('compose');
 });
 
-app.get('/posts/:postTitle', function(req, res) {
-  const requestedTitle = _.lowerCase(req.params.postTitle);
+app.get('/posts/:postId', function(req, res) {
+  const requestedPostId = req.params.postId;
 
-  posts.forEach(post => {
-    const storedTitle = _.lowerCase(post.title);
-
-    if (storedTitle === requestedTitle) {
-      res.render('post', { postTitle: post.title, postContent: post.content });
-    }
+  Post.findOne({_id: requestedPostId}, (err, post) => {
+    res.render('post', { postTitle: post.title, postContent: post.content });
   });
 });
 
 app.post('/compose', function(req, res) {
-  const post = { title: req.body.postTitle, content: req.body.postBody };
-  posts.push(post);
-  res.redirect('/');
+  const post = new Post ({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+  post.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/');
+    }
+  });
 });
 
 app.listen(3000, function() {
